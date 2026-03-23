fd9e05dc152833ed0130d0c21bd2528f62df5e01
Gningue Mamadou <91191658+YRNMAMA@users.noreply.github.com>
2026-03-23 13:17:46 +0000

    feat: aggiunta classe ContactForm e template HTML per la gestione dei contatti

---
+++ rubrica/src/main/java/it/marconi/rubrica/Domain/ContactForm.java
@@ -0,0 +1,20 @@
+package it.marconi.rubrica.Domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ContactForm {
+
+                private String name ; 
+                private String surname; 
+                private String phone ; 
+                private String email; 
+
+
+                
+}
